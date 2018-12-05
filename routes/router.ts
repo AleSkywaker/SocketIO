a39f6398d5d53faa1de769bf07143be17a0ba002
Alex Colombo <alexsandrocolombo@gmail.com>
2018-12-05 12:59:49 +0100

    (initial commit): starting server

---
+++ routes/router.ts
@@ -0,0 +1,20 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/mensajes", (req: Request, res: Response) => {
+  res.json({
+    ok: true,
+    mensaje: "Todo esta bien!!!"
+  });
+});
+router.post("/mensajes/:id", (req: Request, res: Response) => {
+  var nombre = req.body.name;
+  var apellido = req.body.surname;
+  var id = req.params.id;
+  res.json({
+    ok: true,
+    mensaje: `POST - LISTO ${nombre} ${apellido} ${id}`
+  });
+});
+export default router;
