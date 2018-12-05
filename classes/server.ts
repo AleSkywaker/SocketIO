e5738635b304c8221997f0449f1ef0dae739a69e
Alex Colombo <alexsandrocolombo@gmail.com>
2018-12-05 15:00:34 +0100

    (initial commit): starting server

--- classes/server.ts
+++ classes/server.ts
@@ -1,18 +1,37 @@
-import express from 'express';
-import { SERVER_PORT } from '../global/enviroment';
-
+import express from "express";
+import { SERVER_PORT } from "../global/enviroment";
+import socketIO from "socket.io";
+import http from "http";
 
 export default class Server {
-    public app: express.Application;
-    public port: number
+  private static _instance: Server;
+  public app: express.Application;
+  public port: number;
+  private httpServer: http.Server;
 
-    constructor(){
-        this.app = express();
-        this.port = SERVER_PORT;
-    }
+  public io: socketIO.Server;
 
-    start(callback: Function){
-        this.app.listen(this.port, callback)
-    }
+  private constructor() {
+    this.app = express();
+    this.port = SERVER_PORT;
 
+    this.httpServer = new http.Server(this.app);
+    this.io = socketIO(this.httpServer);
+  }
+
+  public static get instance() {
+    return this._instance || (this._instance = new this());
+  }
+
+  private escucharSockets() {
+    console.log("Escuchando conexiones - sockets");
+
+    this.io.on("connection", cliente => {
+      console.log("Cliente conectado");
+    });
+  }
+
+  start(callback: Function) {
+    this.app.listen(this.port, callback);
+  }
 }
